f1ed2798a52f480039a3f9cc186363bae511b418
Anton <antonlashyk@gmail.com>
2020-01-11 13:46:18 +0300

    second task

---
+++ task2/script.js
@@ -0,0 +1,23 @@
+'use strict'
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+let i = 1;
+
+// while (i < 8) {
+    
+//     console.log(i);
+//     i++;
+// }
+
+do {
+
+    console.log(i);
+    i++
+    
+} while(i < 8);
